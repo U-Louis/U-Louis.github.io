caf302ddb5639829dd1edf81c5a45cd720cb71df
lou <69362039+U-Louis@users.noreply.github.com>
2024-02-02 15:41:02 +0100

    Updates

--- assets/nuxt-icon.G9GKhAlR.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./nuxt-icon.vue.5XalegyY.js";import"./entry.K2-VQqMw.js";export{m as default};
---
+++ assets/nuxt-icon.RWDXhFNs.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./nuxt-icon.vue.9s4_9q3C.js";import"./entry.Ji3PGYGV.js";export{m as default};
