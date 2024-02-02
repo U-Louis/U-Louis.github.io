b44023e2ee98f662ce69b67ad45dcebdb262cf9c
lou <69362039+U-Louis@users.noreply.github.com>
2024-02-02 15:26:27 +0100

    Updates

---
+++ assets/nuxt-icon.G9GKhAlR.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./nuxt-icon.vue.5XalegyY.js";import"./entry.K2-VQqMw.js";export{m as default};
--- assets/nuxt-icon.cuwQviyD.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./nuxt-icon.vue.mMX0-5wN.js";import"./entry.paJyEkZ4.js";export{m as default};
