05d9230ae2bfeaf005eebf127cd77ed48ae8e389
Louis <louis.urbani@yahoo.com>
2024-02-16 16:14:21 +0100

    Updates

--- assets/nuxt-icon.GB9o1nAy.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./nuxt-icon.vue.D1LoVa0V.js";import"./entry.9A9tqn9A.js";export{m as default};
---
+++ assets/nuxt-icon.zAHRJgRG.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./nuxt-icon.vue.AMr-P6mj.js";import"./entry.8gf-u-Q7.js";export{m as default};
